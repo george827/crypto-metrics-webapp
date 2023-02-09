96f72cc6265a075b59024ab528aaf87a65a06229
george827 <kinyanjuigeorge827@gmail.com>
2023-02-09 03:19:10 +0300

    added test cases for Home and details page

--- src/components/DetailList.js
+++ src/components/DetailList.js
@@ -13,7 +13,22 @@ export const DetailsList = (props) => {
     <ul className="details-ul">
       {specificDetailToken
         && specificDetailToken.map((crypto) => (
-          <Details key={crypto.id} crypto={crypto} />
+          <Details
+            key={crypto.id}
+            image={crypto.image}
+            currentPrice={crypto.currentPrice}
+            name={crypto.name}
+            symbol={crypto.symbol}
+            marketCap={crypto.symbol}
+            marketCapRank={crypto.marketCap}
+            totalVolume={crypto.totalVolume}
+            high24h={crypto.high24h}
+            low24h={crypto.low24h}
+            priceChange24h={crypto.priceChange24h}
+            marketCapChange24h={crypto.marketCapChange24h}
+            marketCapChangePercentage24h={crypto.marketCapChangePercentage24h}
+            totalSupply={crypto.totalSupply}
+          />
         ))}
     </ul>
   );
